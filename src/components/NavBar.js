7a15a1c85634cbb30d6c49e7a6042962ac2eb613
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-03-13 21:54:36 +0800

    Create NavBar.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import myLogo from '../images/black-in-white-512.png'
+import { Link } from "react-router-dom";
+
+class NavBar extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			home: "navBarButtonsActive",
+			about: "navBarButtonsInactive",
+			portfolio: "navBarButtonsInactive",
+			contact: "navBarButtonsInactive"
+		}
+	}
+
+	changeClass(page) {
+		if(page === "home") {
+			this.setState({
+				home: "navBarButtonsActive",
+				about: "navBarButtonsInactive",
+				portfolio: "navBarButtonsInactive",
+				contact: "navBarButtonsInactive"
+			})
+		}
+		else if(page === "about"){
+			this.setState({
+				home: "navBarButtonsInactive",
+				about: "navBarButtonsActive",
+				portfolio: "navBarButtonsInactive",
+				contact: "navBarButtonsInactive"
+
+			})
+		}
+		else if(page === "portfolio"){
+			this.setState({
+				home: "navBarButtonsInactive",
+				about: "navBarButtonsInactive",
+				portfolio: "navBarButtonsActive",
+				contact: "navBarButtonsInactive"
+			})
+		}
+		else{
+			this.setState({
+				home: "navBarButtonsInactive",
+				about: "navBarButtonsInactive",
+				portfolio: "navBarButtonsInactive",
+				contact: "navBarButtonsActive"
+			})
+		}
+	}
+
+	render() {
+		return (
+			<div id = "navBar" >
+				<div className = "myLogo">
+					<img className = "" src = {myLogo} alt = "My Icon" />
+					&nbsp;
+					&nbsp;
+					&nbsp;
+					<mark>Too</mark>
+				</div>
+				<Link to = "/" onClick = {() => this.changeClass("home")} className = {this.state.home} >
+						<i className = "fa fa-home"></i> Home
+				</Link>
+				<Link to = "/About" onClick = {() => this.changeClass("about")} className = {this.state.about} >
+						<i className="fas fa-info"></i> About
+				</Link>
+				<Link to = "/Portfolio" onClick = {() => this.changeClass("portfolio")} className = {this.state.portfolio} >
+						<i className="fas fa-folder-open"></i> Portfolio
+				</Link>
+				<Link to = "/Contact" onClick = {() => this.changeClass("contact")} className = {this.state.contact} >
+						<i className="fas fa-id-card"></i> Contact
+				</Link>
+			</div>
+		)
+	}
+}
+
+export default NavBar
