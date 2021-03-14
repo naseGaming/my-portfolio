6d2ccf891592133eef24218876fcac1a9512033f
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-03-14 20:15:03 +0800

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,30 @@
-import React from 'react';
- 
-function Portfolio() {
-  return (
-	<form id="Portfolio">
-		Portfolio
-	</form>
-  )
+import React from 'react'
+import './Portfolio.css'
+
+class Portfolio extends React.Component {
+	goTo(page) {
+		if(page === "ClickTitans") {
+			window.open("http://clicktitans.epizy.com/")
+		}
+	}
+
+	render() {
+	  	return (
+			<div id="Portfolio" className = "animate__animated animate__fadeIn" >
+				<div id = "portfolio-content" >
+					<h1>My Personal Projects</h1>
+					<div id = "click-titans" className = "project">
+						<div id = "click-titans-left" className = "leftHalf">
+						</div>
+						<div className = "rightHalf">
+							<p className = "projectTitle" onClick = {() => this.goTo("ClickTitans")} >Click Titans</p>
+							<p className = "projectDescription">A simple game made using HTML, CSS, Native Js, JQuery and PHP</p>
+						</div>
+					</div>
+				</div>
+			</div>
+	  	)
+	}
 }
  
 export default Portfolio;
