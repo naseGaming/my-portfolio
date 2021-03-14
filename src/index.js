03bd2386fc53f3f13defc74657e7f25323abdc95
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-03-14 20:13:04 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom'
+import 'animate.css'
 import './index.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import App from './App'
