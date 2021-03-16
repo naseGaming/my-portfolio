f99ab3d7f52e1757fc873b2ef815a7492cbd7381
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-03-16 11:29:35 +0800

    Update Portfolio.js
    Redesigned the page make it more simple

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,6 +2,33 @@ import React from 'react'
 import './Portfolio.css'
 
 class Portfolio extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			clicktitans: false
+		}
+	}
+
+	onMouseover (e) {
+		this.setState({
+			clicktitans: true
+     	})
+   	}
+
+	onMouseout (e) {
+    	this.setState({
+			clicktitans: false
+    	})
+	}
+
+	showContent(app){
+		if(app === "ClickTitans") {
+			this.setState({
+				clicktitans: true
+			})
+		}
+	}
+
 	goTo(page) {
 		if(page === "ClickTitans") {
 			window.open("http://clicktitans.epizy.com/")
@@ -9,18 +36,39 @@ class Portfolio extends React.Component {
 	}
 
 	render() {
+		const cTContent = this.state.clicktitans
+		let content = ""
+
+		if(cTContent === false) {
+			content = 
+					<div>
+						<div className = "Icon" >
+							<i className = "fas fa-gamepad"></i>
+						</div>
+						<div className = "title" >
+							<strong>Click Titans</strong>
+						</div>
+					</div>
+		}
+		else{
+			content = 
+					<div>
+						<div className = "content" >
+							<p>A simple clicking game resembles tap titans made using HTML, Native Js, JQuery and PHP</p>
+						</div>
+						<div className = "button" >
+							<button className = "VisitApp" onClick = {() => this.goTo("ClickTitans")} >Visit</button>
+						</div>
+					</div>
+		}
+
 	  	return (
 			<div id="Portfolio" className = "animate__animated animate__fadeIn" >
 				<div id = "portfolio-content" >
 					<h1>My Personal Projects</h1>
-					<div id = "click-titans" className = "project">
-						<div id = "click-titans-left" className = "leftHalf">
-						</div>
-						<div className = "rightHalf">
-							<p className = "projectTitle" onClick = {() => this.goTo("ClickTitans")} >Click Titans</p>
-							<p className = "projectDescription">A simple game made using HTML, CSS, Native Js, JQuery and PHP</p>
-						</div>
-					</div>
+					<div onMouseEnter={this.onMouseover.bind(this)} onMouseLeave={this.onMouseout.bind(this)} className = "project" onClick = {() => this.showContent("ClickTitans")} >
+          				{content}
+          			</div>
 				</div>
 			</div>
 	  	)
